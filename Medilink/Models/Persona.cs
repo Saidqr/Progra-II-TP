fb8f7d72b0425c1f6449770d4e70304919d6405b
matty-23 <matu.naser@gmail.com>
2025-10-14 12:23:10 -0300

    Se borraron atributos de Persona (los atributos correspondian a paciente)

--- Medilink/Models/Persona.cs
+++ Medilink/Models/Persona.cs
@@ -4,8 +4,5 @@ namespace Medilink.Models
     {
         public int Id { get; set; }
         public IRol? Rol { get; set; }
-        public string? Documento { get; set; }
-        public DateTime FechaNacimiento { get; set; }
-        public string? Mail { get; set; }
     }
 }
