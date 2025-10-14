0f28800e4d14ca44433328d8fc55e6034b318809
matty-23 <matu.naser@gmail.com>
2025-10-14 08:22:21 -0300

    Se añadio estado

--- Medilink/Models/ConsultaMedica.cs
+++ Medilink/Models/ConsultaMedica.cs
@@ -1,10 +1,11 @@
 namespace Medilink.Models
 {
-    public class Consulta
+    public class ConsultaMedica
     {
         public int Id { get; set; }
         public int IdMedico { get; set; }
         public int IdPaciente { get; set; }
+        public string Estado { get; set; }
         public DateTime Fecha { get; set; }
         
 
