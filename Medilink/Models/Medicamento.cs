0c05f6146c679c812cb3428d4d2594ea928b785c
Said Quioto <said.quioto@uap.edu.ar>
2025-10-14 11:53:00 -0300

    Añadir modelos iniciales de Hospital, Medicamento y Persona

---
+++ Medilink/Models/Medicamento.cs
@@ -0,0 +1,11 @@
+namespace Medilink.Models;
+
+public class Medicamento
+{
+    public int Id { get; set; }
+    public string? Nombre { get; set; }
+    public string? Descripcion { get; set; }
+    public int Cantidad { get; set; } = 0;
+    public DateTime FechaFabricacion { get; set; }
+    public DateTime FechaVencimiento { get; set; } 
+}
