92dca61cffb80bd8ec2141f09586194bbde9b550
matty-23 <matu.naser@gmail.com>
2025-10-15 11:21:21 -0300

    Se implemento la logica de controlador de Medico para la API. Verificar si despues faltan endpoints

---
+++ Medilink/Controllers/MedicosController.cs
@@ -0,0 +1,54 @@
+using Medilink.Models;
+using Medilink.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Medilink.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MedicosController : ControllerBase
+{
+    private readonly MedicoService _medicoService;
+    public MedicosController(MedicoService medicoService)
+    {
+        _medicoService = medicoService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Medico>>> GetAll()
+    {
+        var medicos = await _medicoService.GetMedicos();
+        return Ok(medicos);
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Medico>> GetOneById(int id)
+    {
+        var medico = await _medicoService.GetMedico(id);
+        if (medico == null) return NotFound();
+        return Ok(medico);
+    }
+    [HttpPost]
+    public async Task<ActionResult<Medico>> Create([FromBody] Medico medico)
+    {
+        var medicoNuevo = await _medicoService.AddMedico(medico);
+        return CreatedAtAction(nameof(GetOneById), new { id = medicoNuevo.Id }, medicoNuevo);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Medico>> CompleteUpdate([FromBody] Medico medico, int id)
+    {
+        if (id != medico.Id) return BadRequest();
+        var resultadoMedico = await _medicoService.UpdateMedico(medico);
+        if (!resultadoMedico) return NotFound();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        var medico = await _medicoService.DeleteMedico(id);
+        if (!medico) return NotFound($"No se encontro el medico con ID {id}");
+        else return NoContent();
+    }
+    
+}
