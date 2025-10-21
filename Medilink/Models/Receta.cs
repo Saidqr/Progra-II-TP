0bd593b4decd3ed4a725bc4ad25061f497fa37dd
matty-23 <matu.naser@gmail.com>
2025-10-21 11:55:46 -0300

    Se añadio la fecha de vencimiento al modelo Receta

--- Medilink/Models/Receta.cs
+++ Medilink/Models/Receta.cs
@@ -4,6 +4,7 @@ namespace Medilink.Models
     {
         public int Id { get; set; }
         public int Estado { get; set; }
+        public DateTime FechaVencimiento { get; set; }
         public ConsultaMedica Consulta { get; set; }
         public List<(Medicamento, int)> Medicamentos { get; set; }     
     }
